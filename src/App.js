2658a4cd9b304d6be6f7b7563faa870162cf1ac3
ecdevelop <32962889+ecdevelop@users.noreply.github.com>
2023-02-24 17:30:48 +0800

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,46 +3,24 @@ import './style.css';
 import contacts from './components/contacts.js';
 import Card from './components/Card.jsx';
 
+function createCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      name={contact.name}
+      img={contact.img}
+      tel={contact.tel}
+      email={contact.email}
+    />
+  );
+}
+
 export default function App() {
   return (
     <div>
       <div className="Contacts">
         <h1 className="heading">My Contacts</h1>
-
-        <Card
-          name={contacts[0].name}
-          img={contacts[0].img}
-          tel={contacts[0].tel}
-          email={contacts[0].email}
-        />
-
-        <Card
-          name={contacts[1].name}
-          img={contacts[1].img}
-          tel={contacts[1].tel}
-          email={contacts[1].email}
-        />
-
-        <Card
-          name={contacts[2].name}
-          img={contacts[2].img}
-          tel={contacts[2].tel}
-          email={contacts[2].email}
-        />
-
-        <Card
-          name={contacts[3].name}
-          img={contacts[3].img}
-          tel={contacts[3].tel}
-          email={contacts[3].email}
-        />
-
-        <Card
-          name={contacts[4].name}
-          img={contacts[4].img}
-          tel={contacts[4].tel}
-          email={contacts[4].email}
-        />
+        {contacts.map(createCard)}
       </div>
     </div>
   );
